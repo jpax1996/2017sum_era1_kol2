14ae6cbcc053d3acb844c4547acc47ea52347edf
jpax1996 <duke.jp.freitas@gmail.com>
2017-03-29 14:33:26 +0200

    changes 1.0 joao pedro freitas

--- kol2.py
+++ kol2.py
@@ -14,3 +14,57 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+def interface():
+	print "1) Insert New Student in database"
+	print "2) Change students attendance"
+	print "3) Insert Score to student"
+	print "4) Get students total average score"
+	print "5) Get students average score in class"
+	print "6) Count total attendance of student"
+	print "7) Exit"
+	print(Chose your number :)
+	
+
+def get_input():
+	input = 0
+	while input<1 || input>7 :
+		interface()
+		input = raw_input();
+	return input 
+
+def insert_student()
+
+class student_info:
+	student_attendance = []
+	student_grades = [] []
+
+class classroom:
+	number_students = 0
+	students = []
+	def insert_student(self,name):		
+		self.students[self.number_students] = name
+		self.number_students += 1
+	
+database_classroom = classroom
+database_student = student_info
+
+input = 0
+
+	while input != 7:
+		input = get_input();
+		if(input == 1){
+			print(Please insert name of new student:)
+			new_student = raw_input()
+			database_classroom.insert_student(new_student) 		
+		}elif(input == 2){
+		
+		}elif(input == 3){
+		
+		}elif(input == 4){
+		
+		}elif(input == 5){
+		
+		}elif(input == 6){
+		
+		}
