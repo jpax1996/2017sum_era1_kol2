b3dd0f241d4bc18f6fa39c29ee3d70458683fddc
jpax1996 <duke.jp.freitas@gmail.com>
2017-04-10 20:12:17 +0200

    improv_1

--- kol2.py
+++ kol2.py
@@ -14,57 +14,96 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+from __future__ import division
 
 def interface():
-	print "1) Insert New Student in database"
-	print "2) Change students attendance"
-	print "3) Insert Score to student"
-	print "4) Get students total average score"
-	print "5) Get students average score in class"
-	print "6) Count total attendance of student"
-	print "7) Exit"
-	print(Chose your number :)
+        print "1) Insert new class"
+        print "2) Insert New Student in database"
+        print "3) Change students attendance"
+        print "4) Insert Score to student"
+        print "5) Get classe's total average score"
+        print "6) Get students average score in class"
+        print "7) Count average from every class"
+        print "8) Exit"
+        print "Chose your number :"
 	
 
-def get_input():
-	input = 0
-	while input<1 || input>7 :
-		interface()
-		input = raw_input();
-	return input 
+def get_inpu():
+        in_put = 0
+        while in_put < 1 or in_put > 8:
+                interface()
+                in_put = input()
+                return in_put 
 
-def insert_student()
+def insert_class(newclass):
+        classroom[newclass] = []
 
-class student_info:
-	student_attendance = []
-	student_grades = [] []
-
-class classroom:
-	number_students = 0
-	students = []
-	def insert_student(self,name):		
-		self.students[self.number_students] = name
-		self.number_students += 1
 	
-database_classroom = classroom
-database_student = student_info
+classroom = {}
 
-input = 0
-
-	while input != 7:
-		input = get_input();
-		if(input == 1){
-			print(Please insert name of new student:)
-			new_student = raw_input()
-			database_classroom.insert_student(new_student) 		
-		}elif(input == 2){
-		
-		}elif(input == 3){
-		
-		}elif(input == 4){
-		
-		}elif(input == 5){
-		
-		}elif(input == 6){
-		
-		}
+in_put = 0
+while in_put != 8 :
+        in_put = get_inpu();
+        if in_put == 1:
+                print "Please insert name of new class:"
+                new_class = raw_input()
+                classroom[new_class] = {}
+                print (classroom)
+        elif in_put == 2:
+                print"Please insert name of new student:"
+                new_student = raw_input()
+                print "Please insert name of classroom"
+                new_class = raw_input()
+                classroom[new_class][new_student]= {'attendance':[],'grades':[]}
+        elif in_put == 3:
+                print "Please insert name of class:"
+                classe = raw_input()
+                print"Please insert name of student:"
+                student = raw_input()
+                print"Please insert the students attendance (pres/abs):"
+                att = raw_input()
+                classroom[classe][new_student]['attendance'].append(att)
+        elif in_put == 4:
+                print "Please insert name of class:"
+                classe = raw_input()
+                print"Please insert name of student:"
+                student = raw_input()
+                print"Please grade:"
+                grade = input()
+                classroom[classe][new_student]['grades'].append(grade)
+                
+        elif in_put == 5:
+                cpt = 0
+                som = 0
+                classe = raw_input("Please insert name of class:")
+                for student in classroom[classe][student]:
+                        for grade in classroom[classe][student]['grades']:
+                                cpt += 1
+                                som += grade
+                avg = som/cpt
+                print "The average of the student is:"
+                print (avg)
+        elif in_put == 6:
+                cpt = 0
+                som = 0
+                classe = raw_input("Please insert name of class:")
+                student = raw_input("Please insert name of student:")
+                for grade in classroom[classe][student]['grades']:
+                        cpt += 1
+                        som += grade
+                avg = som/cpt
+                print "The average of the student is:"
+                print (avg)
+        elif in_put == 7:
+                cpt = 0
+                som = 0
+                for classe in classroom[classe]:
+                        for student in classroom[classe][student]:
+                                for grade in classroom[classe][student]['grades']:
+                                        cpt += 1
+                                        som += grade
+                avg = som/cpt
+                print "The average of the student is:"
+                print (avg)
+        print(classroom)
+	
